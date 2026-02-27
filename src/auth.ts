36c86621fe0acdc52d987eb09120030ce6403ea1
Vijendra <pvijendra756@gmail.com>
2026-02-27 10:57:48 +0530

    Made some changes with better-auth config

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,24 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
+import config from "./config/config";
+const pool = new Pool({
+    host: "localhost",
+    port: 5432,
+    user: "app_user",
+    password: "BahubaliJaiHo@8792239654",
+    database: "app_db",
+    ssl: false,
+});
 export const auth = betterAuth({
-    database: new Pool({
-        host: "localhost",
-        port: 5432,
-        user: "app_user",
-        password: "BahubaliJaiHo@8792239654",
-        database: "app_db",
-        ssl: false,
-    }),
+    database: pool,
     emailAndPassword: {
         enabled: true,
+    },
+    baseURL: config.api_domain,
+    secret: process.env.BETTER_AUTH_SECRET,
+    cookies: {
+        // change this flag to true in the case of prod and also set sameSite as none
+        secure: false,
+        sameSite: "lax",
     },
 });
